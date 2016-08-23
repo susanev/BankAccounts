27bc77508a059a7ecb2a122364a24ede6c12a3d8
susan <susan.ra.evans@gmail.com>
2016-08-23 15:03:19 -0700

    throw ArgumentError if Account object created with negative balance

--- Bank.rb
+++ Bank.rb
@@ -3,6 +3,10 @@ module Bank
 		attr_accessor :ID, :balance
 		
 		def initialize(ID, balance=0)
+			if balance < 0
+				raise ArgumentError.new
+			end
+			
 			@ID = ID
 			@balance = balance
 		end
