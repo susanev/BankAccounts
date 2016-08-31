34243384b8ca88e450a498c2bf049fac115d8a67
susan <susan.ra.evans@gmail.com>
2016-08-31 13:12:55 -0700

    fixed expected, actual order for all tests; add tests for savings account

--- bank_test.rb
+++ bank_test.rb
@@ -8,65 +8,86 @@ class BankTest < Minitest::Test
 	def test_account_no_owner
 		acct = Bank::Account.new(13, 1400)
 
-		assert_equal acct.id, 13
-		assert_equal acct.balance, 1400
-		assert_equal acct.owner, nil
+		assert_equal 13, acct.id
+		assert_equal 1400, acct.balance
+		assert_equal nil, acct.owner
 	end
 
 	def test_account_owner
 		owner = Bank::Owner.new("1", "ev", "susan", "123street", "seattle", "wa")
 		acct = Bank::Account.new(13, 1400, owner)
 
-		assert_equal acct.id, 13
-		assert_equal acct.balance, 1400
-		assert_equal acct.owner, owner
+		assert_equal 13, acct.id
+		assert_equal 1400, acct.balance
+		assert_equal owner, acct.owner
 	end
 
 	def test_withdraw
 		acct = Bank::Account.new(13, 1400)
 		
 		acct.withdraw(500)
-		assert_equal acct.balance, 900
+		assert_equal 900, acct.balance
 
 		acct.withdraw(100000)
-		assert_equal acct.balance, 900
+		assert_equal 900, acct.balance
 	end
 
 	def test_deposit
 		acct = Bank::Account.new(13, 1400)
 
 		acct.deposit(5000)
-		assert_equal acct.balance, 6400
+		assert_equal 6400, acct.balance
 	end
 
 	def test_account_csv_all
 		accounts = Bank::Account.all
-		assert_equal accounts.length, 12
-		assert_equal accounts[2].id, 1214
-		assert_equal accounts[6].balance, 9844567
+		assert_equal 12, accounts.length
+		assert_equal 1214, accounts[2].id
+		assert_equal 9844567, accounts[6].balance
 	end
 
 	def test_account_csv_find
-		assert_equal Bank::Account.find(15156).balance, 4356772
-		assert_equal Bank::Account.find(0), nil
+		assert_equal 4356772, Bank::Account.find(15156).balance
+		assert_equal nil, Bank::Account.find(0)
 	end
 
 	def test_owner_csv_all
 		owners = Bank::Owner.all
-		assert_equal owners.length, 12
-		assert_equal owners[2].address, "9 Portage Court\nWinston Salem, North Carolina"
-		assert_equal owners[4].id, "18"
+		assert_equal 12, owners.length
+		assert_equal "9 Portage Court\nWinston Salem, North Carolina", owners[2].address
+		assert_equal "18", owners[4].id
 	end
 
 	def test_owner_csv_find
-		assert_equal Bank::Owner.find(21).name, "Jessica Bell"
-		assert_equal Bank::Owner.find(200), nil
+		assert_equal "Jessica Bell", Bank::Owner.find(21).name
+		assert_equal nil, Bank::Owner.find(200)
 	end
 
 	def test_owner_accounts
 		owner = Bank::Owner.new("25", "ev", "susan", "123street", "seattle", "wa")
-		assert_equal owner.accounts("support/account_owners.csv").length, 1
-		assert_equal owner.accounts("support/account_owners.csv")[0].balance, 0
+		assert_equal 1, owner.accounts("support/account_owners.csv").length
+		assert_equal 0, owner.accounts("support/account_owners.csv")[0].balance
+	end
+
+	def test_savings_account
+		savings_acct = Bank::SavingsAccount.new(25, 0) rescue ArgumentError
+		assert_equal ArgumentError, savings_acct
+
+		savings_acct = Bank::SavingsAccount.new(25, 10)
+		assert_equal 10, savings_acct.balance
+
+		savings_acct.deposit(100)
+		assert_equal 110, savings_acct.balance
+
+		savings_acct.withdraw(50)
+		assert_equal 58, savings_acct.balance
+
+		savings_acct.withdraw(50)
+		assert_equal 58, savings_acct.balance
+
+		assert_equal savings_acct.add_interest(0.25), 0.145
+		assert_equal 58.145, savings_acct.balance
+
 	end
 
 end
